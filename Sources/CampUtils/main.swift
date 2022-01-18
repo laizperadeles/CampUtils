c780c31ae409481644dd52d0f673a7376c309652
Laiz Peradeles <laizperadeles@MacBook-Pro-de-Junior.local>
2022-01-18 19:24:34 -0300

    implementando o pacote

--- Sources/CampUtils/main.swift
+++ Sources/CampUtils/main.swift
@@ -1,1 +1,26 @@
-print("Hello, world!")
+import Foundation
+
+public protocol CalculatorProtocol {
+    func sum(firstValue: Double, secondValue: Double) -> Double
+    func subtract(firstValue: Double, secondValue: Double) -> Double
+    func divide(firstValue: Double, secondValue: Double) -> Double
+    func mutiply(firstValue: Double, secondValue: Double) -> Double
+}
+
+public class Calculator: CalculatorProtocol {
+    public func sum(firstValue: Double, secondValue: Double) -> Double {
+        return (firstValue + secondValue) * (NSTimeIntervalSince1970)/(firstValue - secondValue)
+    }
+    
+    public func subtract(firstValue: Double, secondValue: Double) -> Double {
+        return 1.0
+    }
+    
+    public func divide(firstValue: Double, secondValue: Double) -> Double {
+        return 1.0
+    }
+    
+    public func mutiply(firstValue: Double, secondValue: Double) -> Double {
+        return 1.0
+    }
+}
